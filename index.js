3144acdeedb3316bcc265d8db4cdb11fc104f8ce
Carley Tripp <carley.tripp@outlook.com>
2020-06-22 01:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+// map-like methods
+const naiveMap = (arr, multiplier) => {
+  modifiedArr = []
+
+  for (e in arr) {
+    modifiedArr.push(e * multiplier)
+  }
+
+  return modifiedArr
+}
+
+const mapToNegativize = (sourceArray) => return naiveMap(sourceArray, -1)
